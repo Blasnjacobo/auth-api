7d2e73a7a3fe94484a931368bf84ad85dae88a5b
Blasnjacobo <blasnjacobo05@gmail.com>
2023-12-02 03:19:01 -0700

    adding links5

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 app.use(cors(
+  {
+    origin: ['https://auth-api-back.vercel.app', 'http://localhost:3000'],
+    methods: ['POST', 'GET', 'DELETE'],
+    credentials: true
+  }
 ))
 app.use(express.json())
 
