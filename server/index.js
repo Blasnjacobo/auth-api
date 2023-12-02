bb082a89da5d544ab4d9b246e6cd2b217b916257
Blasnjacobo <blasnjacobo05@gmail.com>
2023-12-02 03:44:36 -0700

    adding links7

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors(
 ))
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Origin", 'no-cors' )
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
   next()
