6d37de3d1ee2e3df0b931740fea413528d38b012
Blasnjacobo <blasnjacobo05@gmail.com>
2023-12-02 03:57:19 -0700

    front done

--- server/index.js
+++ server/index.js
@@ -14,12 +14,6 @@ app.use(cors(
   }
 ))
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", 'no-cors' )
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
-  next()
-})
 
 app.use(express.json())
 
