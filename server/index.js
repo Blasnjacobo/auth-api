550e32db1bea3427519f8b34a963c19e99b6e4f9
Blasnjacobo <blasnjacobo05@gmail.com>
2023-12-02 03:31:16 -0700

    adding links6

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ app.use(cors(
     credentials: true
   }
 ))
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
